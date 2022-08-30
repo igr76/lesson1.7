6e68b5a51524b018ebeeea0c19488553e1abeb2e
igr1 <dfrgth>
2022-08-30 22:02:09 +1000

    lesson 1.7

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        ;
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName;
+        fullName = firstName + "  " + middleName + "  " + lastName;
+        System.out.println("ФИО сотрудника —  " + fullName);
+
+        //перевод регистра
+        System.out.println("Данные ФИО сотрудника для заполнения отчета" +
+                " —  " + fullName.toUpperCase());
+        //замена ё
+        String fullName1 = "Иванов Семён Семёнович";
+        String[] words = fullName1.split("ё");
+        for (int i = 0; i < words.length; i++) {
+            if (i == 0){
+                System.out.print(words[0]);
+            } else
+            System.out.print("е" + words[i]);
+        }
+
+    }
+}
